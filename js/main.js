44d21b282cc8defc7e47bfd2528f25a3db544451
ruomengtu_ghgame <ruomengtu@ghgame.cn>
2025-11-10 18:15:22 +0800

    完善主页

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const categories = document.querySelectorAll(".category");
-  categories.forEach(cat => {
-    cat.addEventListener("click", () => {
-      const page = cat.dataset.page;
-      if (page) {
-        // 加上 pages/ 前缀
-         window.location.href = `./pages/${page}`;
-      }
+    const categories = document.querySelectorAll(".category");
+    categories.forEach(cat => {
+        cat.addEventListener("click", () => {
+            const page = cat.dataset.page;
+            if (page) {
+                // 加上 pages/ 前缀
+                window.location.href = `/RMT-Setting/pages/${page}`;
+            }
+        });
     });
-  });
 });
